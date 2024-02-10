8862e7d8c34697c516bedb98aab218cd73a7b5c7
Ricardo Barbosa <rbrico@gmail.com>
2024-02-10 01:51:08 -0300

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -15,7 +15,6 @@ export default function Home() {
       <form
         action={async () => {
           "use server"
-          console.log("signing out")
           await signOut({
             redirectTo: "/",
           })
