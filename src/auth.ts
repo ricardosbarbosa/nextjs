e230534abd9f97d7390ad9b5cacccb4d60ff4250
Ricardo Barbosa <rbrico@gmail.com>
2024-02-11 16:13:19 -0300

    Update auth.ts

--- src/auth.ts
+++ src/auth.ts
@@ -22,19 +22,10 @@ export const config = {
   theme: {
     logo: "https://next-auth.js.org/img/logo/logo-sm.png",
   },
+  // @ts-ignore
   // adapter: Neo4jAdapter(neo4jSession),
+  // secret: process.env.AUTH_SECRET,
   providers: [
-    // Email({
-    //   server: {
-    //     host: "smtp.gmail.com",//process.env.SMTP_HOST,
-    //     port: 587, //Number(process.env.SMTP_PORT),
-    //     auth: {
-    //       user: "rbrico@gmail.com",//process.env.SMTP_USER,
-    //       pass: "penseemserfeliz"//process.env.SMTP_PASSWORD,
-    //     },
-    //   },
-    //   from: process.env.EMAIL_FROM,
-    // }),
     Google({
       clientId: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
@@ -50,11 +41,51 @@ export const config = {
         }
       }
     }),
+    // {
+    //   id: 'sendgrid',
+    //   type: 'email',
+    //   async sendVerificationRequest({identifier: email, url}) {
+    //     // Call the cloud Email provider API for sending emails
+    //     // See https://docs.sendgrid.com/api-reference/mail-send/mail-send
+    //     const response = await fetch("https://api.sendgrid.com/v3/mail/send", {
+    //       // The body format will vary depending on provider, please see their documentation
+    //       // for further details.
+    //       body: JSON.stringify({
+    //         personalizations: [{ to: [{ email }] }],
+    //         from: { email: "rbrico@gmail.com" },
+    //         subject: "Sign in to Your page",
+    //         content: [
+    //           {
+    //             type: "text/plain",
+    //             value: `Please click here to authenticate - ${url}`,
+    //           },
+    //         ],
+    //       }),
+    //       headers: {
+    //         // Authentication will also vary from provider to provider, please see their docs.
+    //         Authorization: `Bearer ${process.env.SENDGRID_API}`,
+    //         "Content-Type": "application/json",
+    //       },
+    //       method: "POST",
+    //     })
+
+    //     if (!response.ok) {
+    //       const { errors } = await response.json()
+    //       throw new Error(JSON.stringify(errors))
+    //     }
+    //   },
+    // }
   ],
   callbacks: {
     authorized({ request, auth }) {
       const { pathname } = request.nextUrl
-      if (pathname === "/middleware-example") return !!auth
+      // if (pathname === "/") return !!auth
+      // return true
+
+      return !!auth
+    },
+    signIn({ user, account, profile, email, credentials }) {
+      console.log("user", user, "account", account, "profile", profile, "email", email, "credentials", credentials)
       return true
     },
     jwt({ token, user }) {
@@ -81,5 +112,5 @@ export const config = {
   },
 } satisfies NextAuthConfig
 
-
+// @ts-ignore
 export const { handlers, auth, signIn, signOut } = NextAuth(config)
